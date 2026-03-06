14bcb916c2f97e6b778ad276a357c865440303a8
anshu0714 <anshujha14072002@gmail.com>
2026-03-06 18:09:58 +0530

    feat(frontend): initial React setup with routing and auth page placeholders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router";
+import { routes } from "./app.routes.jsx";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  );
+}
+
+export default App;
